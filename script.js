65c2c3688125d02ce67d250547f29db064727f0e
rw <rw@rws-mac-mini.home>
2024-02-26 15:54:24 +0000

    added loader

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+
 
 //Show new quote
 
 function newQuote() {
+    loading();
     //Pick a random quote from api quotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check if author field is blank and replace it with 'unknown'
@@ -23,13 +39,14 @@ function newQuote() {
     } else {
     quoteText.classList.remove('long-quote');
     }
-
-
+    // set quote, hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // // Get quotes from APO 
 async function getQuotes() {
+    loading()
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
@@ -52,3 +69,4 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuotes();
+
